5b2aa3295ad6356efa44ee6f406af3cf95f7d284
fernando <fernando@desk-fedora>
2021-11-10 23:40:28 -0300

    Cadastro de item: Validações obrigatórias. Validar os outros campos e montar o CRUD.

--- src/main/java/local/fmc/gsf/mrd/bean/ItemBean.java
+++ src/main/java/local/fmc/gsf/mrd/bean/ItemBean.java
@@ -9,6 +9,8 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import local.fmc.gsf.mrd.dominio.Dispensa;
 import local.fmc.gsf.mrd.dominio.ItemDeConsumo;
@@ -26,7 +28,8 @@ public class ItemBean implements Serializable {
 	@EJB
 	private DispensaDAO daoDispensa;
 	//Recebe o ID do item selecionado na lista de dispensas
-	private Integer dispensaId = 0;
+	@NotNull(message = "Selecione a dispensa desse item.")
+	private Integer dispensaId;
 	private List<Dispensa> dispensas = new ArrayList<>();
 	private ItemDeConsumo item = new ItemDeConsumo();
 
@@ -65,12 +68,12 @@ public class ItemBean implements Serializable {
 	}
 
 	public Integer getDispensaId() {
-		mensagemGlobal(new FacesMessage(FacesMessage.SEVERITY_INFO,"Info","DispensaId carregado: " + this.dispensaId));
+//		mensagemGlobal(new FacesMessage(FacesMessage.SEVERITY_INFO,"Info","DispensaId carregado: " + this.dispensaId));
 		return dispensaId;
 	}
 
 	public void setDispensaId(Integer dispensaId) {
-		mensagemGlobal(new FacesMessage(FacesMessage.SEVERITY_INFO,"Info","DispensaId definido: " + this.dispensaId));		
+//		mensagemGlobal(new FacesMessage(FacesMessage.SEVERITY_INFO,"Info","DispensaId definido: " + this.dispensaId));		
 		this.dispensaId = dispensaId;
 	}
 }
